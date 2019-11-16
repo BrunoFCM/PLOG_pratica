b0a8eb4a1a5f9d48b24d17aaca444c94c6bb5ce3
BerserkingIdiot <skullventuzelos@hotmail.com>
2019-11-16 15:27:34 +0000

    altered playloop to facilitate ai integration

--- Code/playLoop.pl
+++ Code/playLoop.pl
@@ -9,22 +9,23 @@ updateTurnState([ 0, 2], 0, [ 0, 1]).
 updateTurnState([ 1, 0], 0, [ 0, 1]).
 updateTurnState([ 0, 1], 0, [ 1, 0]).
 
-getInput(0, _, 1, Xcoord, Ycoord):-
+/* getInput(+Board, +P1, +P2, +CurrPlayer, +Turns, -Xcoord, -Ycoord) */
+getInput( Board, 0, _, 1, _, Xcoord, Ycoord):-
     getPlayerInput(1, Xcoord, Ycoord).
 
-getInput(_, 0, 2, Xcoord, Ycoord):-
+getInput( Board, _, 0, 2, _, Xcoord, Ycoord):-
     getPlayerInput(2, Xcoord, Ycoord).
 
 /*TODO: make getInput for bots*/
 
-doTurn(Board, Player1, Player2, CurrPlayer, NewBoard, Cut):-
-    repeat.
-    getInput(Player1, Player2, CurrPlayer, Xcoord, Ycoord),
+doTurn(Board, Player1, Player2, CurrPlayer, Turns, NewBoard, Cut):-
+    repeat,
+    getInput(Board, Player1, Player2, CurrPlayer, Turns, Xcoord, Ycoord),
     executePlay(Board, CurrPlayer, Xcoord, Ycoord, NewBoard, Cut).
 
 playLoop(Board, Player1, Player2, Turns):-
     getCurrentPlayer(Turns, CurrPlayer),
-    doTurn(Board, Player1, Player2, CurrPlayer, NewBoard, Cut),
+    doTurn(Board, Player1, Player2, CurrPlayer, Turns, NewBoard, Cut),
     /*TODO: display */
     updateTurnState(Turns, Cut, NewTurns),
     \+ checkGameEnd(NewBoard, CurrPlayer),
