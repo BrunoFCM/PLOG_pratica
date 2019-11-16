67ef64c5d983586956a7c4ff8236876ee64a12e2
BerserkingIdiot <skullventuzelos@hotmail.com>
2019-11-16 23:15:11 +0000

    Commented code

--- Code/getPlayerInput.pl
+++ Code/getPlayerInput.pl
@@ -1,3 +1,6 @@
+
+/* getPlayerInput(+Player, -Xcoord, -Ycoord) */
+/* as get_char leaves the end of line character in the buffer, skip_line is used */
 getPlayerInput(1, Xcoord, Ycoord):-
     write('Player 1, please insert valid coordinates for your next piece (X,Y)'),
     get_char(Xcoord), get_char(','), get_char(Ycoord), skip_line.
--- Code/playLoop.pl
+++ Code/playLoop.pl
@@ -1,7 +1,11 @@
 
+/* getCurrentPlayer(+Turns, -CurrentPlayer) */
+/* Reads the turn list and determines who plays next. */
 getCurrentPlayer([ _, 0], 1).
 getCurrentPlayer([ 0, _], 2).
 
+/* updateTurnState(+Turns, +Cut, -NewTurns) */
+/* Determines the amount of remaining moves each player has, depending on whether there was a cut. */
 updateTurnState([ _, 0], 1, [ 0, 2]).
 updateTurnState([ 0, _], 1, [ 2, 0]).
 updateTurnState([ 2, 0], 0, [ 1, 0]).
@@ -10,6 +14,7 @@ updateTurnState([ 1, 0], 0, [ 0, 1]).
 updateTurnState([ 0, 1], 0, [ 1, 0]).
 
 /* getInput(+Board, +P1, +P2, +CurrPlayer, +Turns, -Xcoord, -Ycoord) */
+/* Gets the coordinates for the next move from the current player. */
 getInput( _, 0, _, 1, _, Xcoord, Ycoord):-
     getPlayerInput(1, Xcoord, Ycoord).
 
@@ -18,11 +23,16 @@ getInput( _, _, 0, 2, _, Xcoord, Ycoord):-
 
 /*TODO: make getInput for bots*/
 
+
+/* doTurn(+Board, +P1, +P2, +CurrPlayer, Turns, -NewBoard, -Cut) */
+/* Prompts the current player to make his/her move and modifies the board accordingly. */
 doTurn(Board, Player1, Player2, CurrPlayer, Turns, NewBoard, Cut):-
     repeat,
     getInput(Board, Player1, Player2, CurrPlayer, Turns, Xcoord, Ycoord),
     executePlay(Board, CurrPlayer, Xcoord, Ycoord, NewBoard, Cut).
 
+/* playLoop(+Board, +P1, +P2, +Turns) */
+/* Main recursive loop of the game. */
 playLoop(Board, Player1, Player2, Turns):-
     getCurrentPlayer(Turns, CurrPlayer),
     doTurn(Board, Player1, Player2, CurrPlayer, Turns, NewBoard, Cut),
