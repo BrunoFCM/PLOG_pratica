c0debe4b773ff1846e4b1493712aa84d65acf572
BrunoFCM <bruno_micaelo@hotmail.com>
2019-11-16 18:47:27 +0000

    Most of the code has been implemented, requires merge and testing

--- Code/gameAI.pl
+++ Code/gameAI.pl
@@ -1,4 +1,6 @@
 :-use_module(library(random)).
+:-include('gameLogic.pl').
+
 :-dynamic(distance/2).
 
 /*------------------------------------------------------------------------------------------------------*/
@@ -17,7 +19,9 @@ findAllEmpty([Row|BoardPieces], X, Y, Points):-
 
 findAllEmpty(_, 0, []).
 
-findAllEmpty([0|Row], X, XValues):-
+findAllEmpty([0|Row], X, [X|XValues]):-
+    Xn is X - 1,
+    findAllEmpty(Row, Xn, Xvalues).
 
 /*------------------------------------------------------------------------------------------------------*/
 
@@ -34,53 +38,191 @@ subtractIndex([Element|List], Index, Value, [Element|NewList]):-
 
 /*------------------------------------------------------------------------------------------------------*/
 
-getIdealPath(BoardState, Player, Path)
+dijkstra([]).
+
+dijkstra([Node|NodeList]):-
+    visit([Node], 1),
+    dijkstra(NodeList).
+
+% Stops when the node list is empty
+visit([], _).
+
+visit([[X,Y] | Nodes], Distance):-
+    \+visited(X,Y),
+
+    distance([X,Y], OlderDistance),
+    Distance < OlderDistance,
+
+    % From this point on, this predicate does not fail, assuring the retraction after the assertion
+    asserta(visited(X,Y)),
+    
+    findall(Adjacent, isAdjacent([X,Y], Adjacent), Adjacents),
+    findall(Diagonal, isDiagonal([X,Y], _, Diagonal), Diagonals),
+    append(Adjacents, Diagonals, NewNodes),
+
+    NextDistance is Distance + 1,
+    visit(NewNodes, NextDistance),
+
+    retract(visited(X,Y)).
+
+% Visit very Node in the Node list
+visit([_|Nodes], Distance):-
+    visit(Nodes, Distance).
 
 /*------------------------------------------------------------------------------------------------------*/
 
-getStartingTurn(1, [1,0]).
-getStartingTurn(2, [0,1]).
+% Player piece found
+initDistancesRow([Player|Row], X, Y, Player):-
+    distance([X,Y], 1),
+    Xn is X + 1,
+    initDistancesRow(Row, Xn, Y, Player).
 
-opponent(1,2).opponent(2,1).
-    
+% Empty starting Postion for player 1
+initDistancesRow([0|Row], 1, Y, 1):-
+    distance([1,Y], 1),
+    Xn is X + 1,
+    initDistancesRow(Row, Xn, Y, Player).
+
+% Empty starting Postion for player 2
+initDistancesRow([0|Row], X, 1, 2):-
+    distance([X,1], 1),
+    Xn is X + 1,
+    initDistancesRow(Row, Xn, Y, Player).
+
+% Empty position found
+initDistancesRow([0|Row], X, Y, Player):-
+    distance([X,Y], 99),
+    Xn is X + 1,
+    initDistancesRow(Row, Xn, Y, Player).
+
+% Opposing player piece found
+initDistancesRow([_|Row], X, Y, Player):-
+    Xn is X + 1,
+    initDistancesRow(Row, Xn, Y, Player).
+
+initDistances([Row|BoardState], Y, Player):-
+    initDistancesRow(Row, 1, Y, Player),
+    Yn is Y + 1,
+    initDistances(BoardState, Yn, Player).
+
+/*------------------------------------------------------------------------------------------------------*/
+
+getClosestNode([Node], Node).
+
+getClosestNode([Node|List], ClosestNode):-
+    getClosestNode(List, ClosestNode),
+    distance(Node, Distance1),
+    distance(ClosestNode, Distance2),
+    Distance1 > Distance2.
+
+getClosestNode([Node|List], Node).
+
+/*------------------------------------------------------------------------------------------------------*/
+
+makePath(StartingPoint, []):-
+    distance(StartingPoint, 1).
+
+makePath(StartingPoint, [StartingPoint|Path]):-
+    findall(Adjacent, isAdjacent(StartingPoint, Adjacent), Adjacents),
+    findall(Diagonal, isDiagonal(StartingPoint, _, Diagonal), Diagonals),
+    append(Adjacents, Diagonals, NextPoints),
+    getClosestNode(NextPoints, NextNode),
+    makePath(NextNode, Path).
+
+/*------------------------------------------------------------------------------------------------------*/
+
+getIdealPath(BoardState, Player, Path):-
+    initDistances(BoardState, 1, Player),
+    findAll(Node, distance(Node, 1), StartingNodes),
+    dijkstra(StartingNodes),
+    subtractRandomIndex(StartingNodes, StartingPoint),
+    makePath(StartingPoint, Path).
+
+/*------------------------------------------------------------------------------------------------------*/
+
+isEmptyList([]).
+
+handlePaths(PlayerPath, OpponentPath, Intersected):-
+    intersect(PlayerPath, OpponentPath, Intersected),
+    \+isEmptyList(Intersected).
+
+handlePaths(PlayerPath, OpponentPath, PlayerPath):-
+    length(PlayerPath, PlayerLength),
+    length(OpponentPath, OpponentLength),
+    PlayerLength < OpponentLength.
+
+handlePaths(_, OpponentPath, OpponentPath).
+
+/*------------------------------------------------------------------------------------------------------*/
+
+opponent(1,2).
+opponent(2,1).
+
 simulatePaths(_, Turns, BoardState, _, []):-
-    getCurrentPlayer(_, Player),
+    getCurrentPlayer(Turns, Player),
     checkGameEnd(BoardState, Player).
 
 simulatePaths(Depth, Turns, BoardState, Play, []):-
+    getCurrentPlayer(Turns, Player),
     opponent(Player, Opponent),
-/*TODO*/
     getIdealPath(BoardState, Player, PlayerPath),
     getIdealPath(BoardState, Opponent, OpponentPath),
-
     handlePaths(PlayerPath, OpponentPath, Path),
-
     subtractRandomIndex(Path, Play, AlternativePlays),
+    simulateFollowingPaths(Depth,).
 
-    doTurn(Board, Player1, Player2, CurrPlayer, NewBoard, Cut),
-
+/* These two predicates have the purpose of trying every alternative play*/
+simulatePaths(Depth, Turns, BoardState, _, [[Xcoord,Ycoord]|AlternateList]):-
+    getCurrentPlayer(Turns, Player),
+    executePlay(BoardState, Player, Xcoord, Ycoord, NewBoard, Cut).
     updateTurnState(Turns, Cut, NewTurns),
-
     DepthN is Depth - 1,
-    /+simulatePaths(DepthN, OpponentPath, BoardState, AlternativePlays).
+    /+simulatePaths(DepthN, OpponentPath, BoardState, _, []).
 
-simulatePaths(Depth, Turns, BoardState, Play, [Play|AlternateList]):-
-    doTurn(Board, Player1, Player2, CurrPlayer, NewBoard, Cut),
+simulatePaths(Depth, Turns, BoardState, _, [_|AlternateList]):-
+    simulatePaths(Depth, Turns, BoardState, _, AlternateList).
 
+/*------------------------------------------------------------------------------------------------------*/
+    
+/*Considering two turn plays*/ 
+simulateFollowingPaths(Depth, [_,2], BoardState, AlternativePlays):-
+    Depth > 0,
+    executePlay(Board, [0,2], Xcoord, Ycoord, NewBoard, Cut),
     updateTurnState(Turns, Cut, NewTurns),
-
     DepthN is Depth - 1,
-    /+simulatePaths(DepthN, OpponentPath, BoardState, AlternativePlays).
+    /*Simulating the next move as the same player*/
+    simulatePaths(DepthN, NewTurns, BoardState, AlternativePlays).
 
-simulatePaths(Depth, Turns, BoardState, Play, [_|AlternateList]):-
-    simulatePaths(Depth, Turns, BoardState, Play, AlternateList).
+simulateFollowingPaths(Depth, [2,_], BoardState, AlternativePlays):-
+    Depth > 0,
+    executePlay(Board, [2,0], Xcoord, Ycoord, NewBoard, Cut),
+    updateTurnState(Turns, Cut, NewTurns),
+    DepthN is Depth - 1,
+    simulatePaths(DepthN, NewTurns, BoardState, AlternativePlays).
+
+% Considering one turn plays
+simulateFollowingPaths(Depth, Turns, BoardState, AlternativePlays):-
+    Depth > 0,
+    executePlay(Board, Turns, Xcoord, Ycoord, NewBoard, Cut),
+    updateTurnState(Turns, Cut, NewTurns),
+    DepthN is Depth - 1,
+    /*Simulating the next move as the opposing player*/
+    \+simulatePaths(DepthN, NewTurns, BoardState, AlternativePlays).
 
 /*------------------------------------------------------------------------------------------------------*/
 
-getAIinput(Depth, Player, BoardState, Input):-
-    simulatePaths(Depth, Player, BoardState, Input, []).
+getAIinput(Depth, Turns, BoardState, Input):-
+    simulatePaths(Depth, Turns, BoardState, Input, []).
 
-getAIinput(_, Player, [BoardPieces,_], Input):-
+getAIinput(_, _, [BoardPieces,_], Input):-
     findAllEmpty(BoardPieces, PossiblePlays),
     subtractRandomIndex(PossiblePlays, Input, _).
 
+/*------------------------------------------------------------------------------------------------------*/
+
+getLevel(1, [Player1, _], Player1).
+getLevel(2, [_, Player2], Player2).
+
+getInput(BoardState, Turns, Player1, Player2, CurrPlayer, Xcoord, Ycoord):-
+    getLevel(CurrPlayer, [Player1,Player2], Depth),
+    getAIinput(Depth, Turns, BoardState, [Xcoord, Ycoord]).
